53bb0e5de03d934e71c2eb840b2d81e763ceb0af
shreyanshishukla <shreyashi1217@gmail.com>
2023-08-31 10:20:52 +0530

    updating

--- src/actions/NewsActions.js
+++ src/actions/NewsActions.js
@@ -1,7 +1,6 @@
 export const add = (payload) => ({ type: "addnews", payload });
 export const deletenews = (payload) => ({ type: "deletenews", payload });
 export const deleteallnews = () => ({ type: "clearallnews" });
-export const updatenews = (payload) => ({ type: "updatenews", payload });
 export const clearallnewsforcity = (payload) => ({
   type: "clearnewsforcity",
   payload,
